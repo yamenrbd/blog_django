b992b130a47e49a77696dec5297a15c6a9a7ff5b
yamenrbd <yamenrbd@gmail.com>
2023-05-21 10:41:24 +0000

    add schema and documentations

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import UserViewSet, PostViewSet
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path("", PostList.as_view(), name="post_list"),
-    path("<int:pk>", PostDetail.as_view(), name="post_detail"),
-]
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
+urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
 from .models import Post
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
-from .serializers import PostSerializer
+
+# from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAdminUser
+
 
 # Create your views here.
 
 
-class PostList(ListCreateAPIView):
+class PostViewSet(ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostDetail(RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+class UserViewSet(ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+# class UserDetail(RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
